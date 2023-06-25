0eb20bf99b1eec918ff3d7509fb855fc5b469210
chris <christopherdeleclusepro@gmail.com>
2023-06-25 22:54:31 +0200

    remove dal and bll

--- Contact.BLL/Repositories/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-using Contact.BLL.Entities;
-
-namespace Contact.BLL.Repositories;
-
-public interface IAuthRepository
-{
-    public void Register(UserEntity user);
-    public UserEntity? Login(string email, string password);
-}
--- Contact.DAL/Repositories/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-using Contact.DAL.Entities;
-
-namespace Contact.DAL.Repositories;
-
-public interface IAuthRepository
-{
-    public void Register(UserEntity user);
-    public UserEntity? Login(string email, string password);
-}
